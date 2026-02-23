336309093d928f4243545bb0e8f6ae0f920f2a5c
rukarajinbelajar <maar1101dnd@gmail.com>
2026-02-23 16:29:01 +0700

    menambahkan langkah3

--- hitungluas.cpp
+++ hitungluas.cpp
@@ -5,7 +5,11 @@ int main(){ //mulai langkah
     int p, l,luas;
 
     //langkah2
-    cout << "masukkan  panjang" ;
+    cout << "masukkan  panjang:" ;
     cin >>p;
+
+    //langkah3
+    cout << "masukkan lebar:" ;
+    cin >>l;
 
 }
